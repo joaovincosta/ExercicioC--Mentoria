90ef829eda196df2a4bff4f270554b2534338849
JOAO COSTA <joao.costa408@etec.sp.gov.br>
2024-08-21 15:42:18 -0300

    Arrays e ReadLine e WriteLine

---
+++ ComentariosWriteLineEReadLine/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Este é um tipo de comentário em linha
+
+/*    
+ 
+ Este é um tipo de comentário em bloco
+
+ */
+
+// Adicionando uma variável nome pessoa
+string nomePessoa = "João";
+
+/* 
+ Adicionando uma variável salário pessoa
+ Adicionando uma variável idade pessoa
+ */
+double salario = 229.90;
+int idade = 15;
+
+// Mostrando mensagem no console para escrever o nome
+Console.WriteLine("Digite o nome da pessoa: ");
+
+// ReadLine
+nomePessoa = Console.ReadLine();
+
+// WriteLine exibe mesagens na tela
+Console.WriteLine("Nome: " + nomePessoa);
+Console.WriteLine("Idade: " + idade);
+Console.WriteLine("Salario: " + salario);
